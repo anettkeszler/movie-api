7d63f752f64b7bb1f5fd25ce0e66014811da82b9
AnettKeszler <anett.dr.keszler@gmail.com>
2020-09-15 15:36:38 +0200

    create search function to narrow results

--- src/Components/Mainpage/Searchbar.js
+++ src/Components/Mainpage/Searchbar.js
@@ -1,29 +1,53 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
+import axios from 'axios';
 
 const Searchbar = () => {
-  //   const [state, setState] = useState({
-  //     input: "",
-  //     results: [],
-  //     selected: {},
-  //   });
-  //   const handleInput = (e) => {
-  //     let input = e.target.value;
-  //     setState((prevState) => {
-  //       return { ...prevState, input: input };
-  //     });
-  //   };
+  const [state, setState] = useState({
+    input: '',
+    results: [],
+    selected: {},
+  });
 
-  //   useEffect(() => {});
+  const url = `http://www.omdbapi.com/?apikey=5848bcce`;
+
+  const search = e => {
+    if (e.preventDefault) {
+      axios(url + '&s=' + state.input).then(data => {
+        console.log(data.data.Search);
+      });
+    }
+  };
+  // const search = e => {
+  //   if (e.preventDefault) {
+  //     axios.get(url).then((res) => {
+  //       setState(res.data);
+  //     });
+  //   }, []);
+
+  const handleInput = e => {
+    let input = e.target.value;
+    setState(state => {
+      return { ...state, input: input };
+    });
+
+    console.log(state.input);
+  };
+
+  // useEffect(() => {});
 
   return (
-    <section className="search-bar">
+    <section className='search-bar'>
       <input
-        className="search-box"
-        type="text"
-        placeholder="Search for a movie"
+        onChange={handleInput}
+        className='search-box'
+        type='text'
+        placeholder='Search for a movie'
       />
-      <button className="search-button">Search</button>
+      <button onClick={search} className='search-button'>
+        Search
+      </button>
+      {/* <Searchbar handleInput={handleInput} /> */}
     </section>
   );
 };
