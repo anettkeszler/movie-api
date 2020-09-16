756a79e4142bcdf0aa92bcb01d4b7876a3c04c5d
noir <tulola14@gmail.com>
2020-09-16 11:17:30 +0200

    Context Api

--- src/Components/Mainpage/Mainpage.js
+++ src/Components/Mainpage/Mainpage.js
@@ -3,12 +3,12 @@ import "./style.css";
 import Searchbar from "./Searchbar";
 import Movielist from "../Movielist/Movielist";
 
-function Mainpage({ movieData }) {
+function Mainpage() {
   return (
     <div className="mainpage">
       <h1 className="title">Movie Api</h1>
       <Searchbar />
-      <Movielist movieData={movieData} />
+      <Movielist />
     </div>
   );
 }
--- src/Components/Movielist/Movielist.js
+++ src/Components/Movielist/Movielist.js
@@ -2,10 +2,10 @@ import React from "react";
 import MovieCard from "../MovieCard/MovieCard";
 import "./style.css";
 
-export default function Movielist({ movieData }) {
+export default function Movielist() {
   return (
     <div className="container">
-      <MovieCard movieData={movieData} />
+      <MovieCard />
     </div>
   );
 }
