82c7d11ac3e6055b9400de639b9242da53b6f7ea
Bmarci <bezzeghmarci@gmail.com>
2020-09-17 21:52:23 +0200

    final product week #1

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,10 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function Footer() {
   return (
-    <div className="footer">
-      <p>Team xy, </p>
+    <div className='footer'>
+      <p>Team M&M's, </p>
       <p>Members: Netti,Marci,Szotyi</p>
     </div>
   );
