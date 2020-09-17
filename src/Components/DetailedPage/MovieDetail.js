ffe6cc0259250f5e62c4e67c68ac6e4804ecc493
AnettKeszler <anett.dr.keszler@gmail.com>
2020-09-17 12:59:35 +0200

    functioning movie detail page

--- src/Components/DetailedPage/MovieDetail.js
+++ src/Components/DetailedPage/MovieDetail.js
@@ -6,6 +6,7 @@ export function MovieDetail(props) {
   const [movieDetail, setMovieDetail] = useState([]);
   const imdbId = props.match.params.imdbID;
   console.log(imdbId);
+  console.log(movieDetail);
 
   const url = `http://www.omdbapi.com/?i=${imdbId}&apikey=8a2bf193`;
 
@@ -13,20 +14,18 @@ export function MovieDetail(props) {
     axios.get(url).then(res => setMovieDetail(res.data));
   }, [url]);
 
-  return movieDetail.map(movie => {
-    return (
-      <div className='detail-container'>
-        {/* {movie.Poster === 'N/A' ? ( */}
-        {/* <img className='card-img' src={defaultPoster} alt='' />
+  return (
+    <div className='detail-container'>
+      {/* {movie.Poster === 'N/A' ? ( */}
+      {/* <img className='card-img' src={defaultPoster} alt='' />
         ) : ( */}
-        <img className='card-img' src={movie?.Poster} alt='' />
-        {/* )} */}
-        <div className='card-title'>
-          {movie.Title} ({movie.Year})
-        </div>
+      <img className='card-img' src={movieDetail?.Poster} alt='' />
+      {/* )} */}
+      <div className='card-title'>
+        {movieDetail.Title} ({movieDetail.Year})
       </div>
-    );
-  });
+    </div>
+  );
 }
 
 export default MovieDetail;
