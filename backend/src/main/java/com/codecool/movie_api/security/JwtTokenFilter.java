0869ac36ea03ec41c41011d03152a29cf7be07e0
AnettKeszler <anett.dr.keszler@gmail.com>
2020-11-05 14:34:16 +0100

    add helper methods to validate tokens

--- backend/src/main/java/com/codecool/movie_api/security/JwtTokenFilter.java
+++ backend/src/main/java/com/codecool/movie_api/security/JwtTokenFilter.java
@@ -1,7 +1,43 @@
 package com.codecool.movie_api.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
 
 @Component
-public class JwtTokenFilter {
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final JwtTokenServices jwtTokenServices;
+
+    public static final String TOKEN = "token";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        Optional<Cookie> jwtToken =
+                Arrays.stream(Optional.ofNullable(request.getCookies()).orElse(new Cookie[]{}))
+                        .filter(cookie -> cookie.getName().equals(TOKEN))
+                        .findFirst();
+        if (jwtToken.isPresent()) {
+            UsernamePasswordAuthenticationToken userToken = jwtTokenServices.validateTokenAndExtractUserSpringToken(jwtToken.get().getValue());
+
+            SecurityContextHolder.getContext().setAuthentication(userToken);
+
+        }
+        filterChain.doFilter(request, response);
+    }
 }
