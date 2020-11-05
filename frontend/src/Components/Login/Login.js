efd42fbe604ac140b8c8be77bdef7cb72bfdcc63
AnettKeszler <anett.dr.keszler@gmail.com>
2020-11-05 16:29:13 +0100

    refactor logout endpoint

--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -14,7 +14,7 @@ export default function Login() {
 
   const sendData = () => {
     const userData = {
-      userName: userName,
+      username: userName,
       password: password,
     };
     console.log(userData);
